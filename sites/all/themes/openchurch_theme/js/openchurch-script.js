3c03ea999fb26ae7dd518c08d87372225ba32f5d
Mike Lewis <mlewis@beracha.org>
2015-03-27 14:10:59 -0400

    Upgraded the OpenChurch Theme

--- sites/all/themes/openchurch_theme/js/openchurch-script.js
+++ sites/all/themes/openchurch_theme/js/openchurch-script.js
@@ -4,8 +4,5 @@
     if (jQuery().equalHeights) {
       $("#content-inner div.equal-heights div.content").equalHeights();
     }
-
-    //adds rounded corners via js for IE
-    $('.rounded-corners .inner, .rounded-corners .pane-content').corner(".3em");
   });
-})(jQuery); 
+})(jQuery);
